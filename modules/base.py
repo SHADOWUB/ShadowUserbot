28ea12e94697316396cfa91724c5a6f87a717266
Yashiro <customfailure00004@gmail.com>
2024-04-17 23:00:22 +0530

    Create base.py

---
+++ modules/base.py
@@ -0,0 +1,23 @@
+from telethon.sync import TelegramClient, events
+import asyncio
+from telethon import TelegramClient, events, Button
+import telethon.sync
+from telethon.tl.functions.messages import ForwardMessagesRequest
+from telethon import functions
+from telethon.errors import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelRequest
+from telethon.tl.types import ChannelParticipantCreator
+from telethon.tl.functions.messages import ForwardMessagesRequest
+from telethon.tl.functions.messages import SendMessageRequest
+from telethon.tl.types import InputPeerChannel
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel
+
+
+async def outgoing_commands(event):
+    if event.raw_text == ".alive":
+        await event.edit("Yes Sax Sux")
+
+async def editing_messages(event):
+    pass
