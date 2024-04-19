435d9cfa39a5696e79ce9f3a7ff8c72f9a3a0394
Yashiro <customfailure00004@gmail.com>
2024-04-19 20:13:41 +0530

    Update base.py

--- modules/base.py
+++ modules/base.py
@@ -13,11 +13,14 @@ from telethon.tl.functions.messages import ForwardMessagesRequest
 from telethon.tl.functions.messages import SendMessageRequest
 from telethon.tl.types import InputPeerChannel
 from telethon.tl.types import PeerUser, PeerChat, PeerChannel
+from modules.gemini import *
 
 
 async def outgoing_commands(event):
     if event.raw_text == ".alive":
         await event.edit("Yes Sax Sux")
+    elif event.raw_text.lower().startswith("yo gemini")
+        await gemini_do(event)
 
 async def edited_handler(event):
     pass
